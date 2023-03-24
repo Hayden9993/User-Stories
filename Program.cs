0b97d07ee01b12ed2c3cef86d3eb96f9250ed4fa
Hayden Smith (1000119848) <SMITHT7@student.op.ac.nz>
2023-03-24 14:26:08 +1300

    feat: add menus for allowing user functionality

--- Program.cs
+++ Program.cs
@@ -2,13 +2,53 @@
 {
     internal class Program
     {
+        private static string input;
+        private static Tour tours;
+        
         static void Main(string[] args)
         {
-            int input;
-            Console.WriteLine("Welcome to Heavenly Visions Tour Manager!\nWhat would you like to do?\n");
-            Console.WriteLine("View Tours [1]\nAdd Tour[2]\nModify Tour[3]");
-            
+            //load tour profile
+
+            tours.append(tour);
+
+            //Open the main menu
+            MainMenu();
+        }
+
+        static void MainMenu() //asks the user what they want to do
+        {
+            Console.Clear();
+            Console.WriteLine("Heavenly Visions Tour Manager\n\nWhat would you like to do?\n");
+            Console.WriteLine("View Tours [1]\nAdd New Tour[2]\nExit[3]");
+
             input = Console.ReadLine();
+            switch (input)
+            {
+                case "1": //view tours
+                    //display tours
+                    break;
+
+                case "2": //add tour
+                    Console.Clear();
+                    Console.WriteLine("Heavenly Visions Tour Manager\n\nPlease Enter a name:\n");
+                    tours.Append(new Tour());
+                    break;
+
+                case "3": //exit
+                    Environment.Exit(0);
+                    break;
+
+                default: //bad input
+                    Console.WriteLine("Incorrect Input!\nPlease try again!\n\n> ");
+                    Console.ReadLine();
+                    MainMenu();
+                    break;
+            }
+        }
+
+        static void ViewTours() //view all tours
+        {
+                
         }
     }
 }
